562d1d011afbfb54c4ec11d17095cab253d1ee7e
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2021-09-01 15:57:23 +0900

    update main.py

--- paper/main.py
+++ paper/main.py
@@ -49,49 +49,39 @@ def topz_handler():
 
 
 @app.post("/paper")
-def create_paper_handler():
-    return {
-        "id": 1,
-        "author_id": [2, 5, 8],
-        "title": "my second paper",
-        "keywords": ["cloud"],
+def create_paper_handler(paper: Paper):
+    my_paper = {
+        "id": 123,
+        "author_id": [2, 5],
+        "title": "分散トレーシングのためのログ検索の高速化",
+        "keywords": ["分散", "トレーシング", "ログ", "検索", "高速化"],
+        "label": "CDSL-TR-051",
         "categories_id": [3],
-        "abstract": "this is a pen.",
-        "url": "https://example.com/yyy",
+        "abstract": "分散トレーシングは,マイクロサービスアーキテクチャでログによる動作の解析を実現する...",
+        "url": "https://drive.google.com/file/d/1feZlqWejgqf8zpWQBQOSpr5JXYkPQL4t/view",
         "thumbnail_url": "https://example.com/zzz",
-        "created_at": "1985-06-24T23:20:50.52Z",
-        "updated_at": "2021-04-18T23:20:50.52Z",
+        "created_at": datetime(1985, 6, 24, 23, 50, 52),
+        "updated_at": datetime(2021, 2, 4, 13, 52, 22)
     }
+    return Paper(**my_paper)
 
 
 @app.get("/paper")
 def read_papers_handler():
-    return [
-        {
-            "id": 1,
-            "author_id": [2, 3, 4],
-            "title": "my original paper",
-            "keywords": ["cloud", "network"],
-            "categories_id": [1, 2],
-            "abstract": "this is a paper.",
-            "url": "https://example.com/xxx",
-            "thumbnail_url": "https://example.com/zzz",
-            "created_at": "1985-07-12T23:20:50.52Z",
-            "updated_at": "2021-05-12T23:20:50.52Z",
-        },
-        {
-            "id": 2,
-            "author_id": [2, 5, 8],
-            "title": "my second paper",
-            "keywords": ["cloud"],
-            "categories_id": [3],
-            "abstract": "this is a pen.",
-            "url": "https://example.com/yyy",
-            "thumbnail_url": "https://example.com/zzz",
-            "created_at": "1985-06-24T23:20:50.52Z",
-            "updated_at": "2021-04-18T23:20:50.52Z",
-        }
-    ]
+    my_paper = {
+        "id": 22,
+        "author_id": [2, 5],
+        "title": "分散トレーシングのためのログ検索の高速化",
+        "keywords": ["分散", "トレーシング", "ログ", "検索", "高速化"],
+        "label": "CDSL-TR-051",
+        "categories_id": [3],
+        "abstract": "分散トレーシングは,マイクロサービスアーキテクチャでログによる動作の解析を実現する...",
+        "url": "https://drive.google.com/file/d/1feZlqWejgqf8zpWQBQOSpr5JXYkPQL4t/view",
+        "thumbnail_url": "https://example.com/zzz",
+        "created_at": datetime(1985, 6, 24, 23, 50, 52),
+        "updated_at": datetime(2021, 2, 4, 13, 52, 22)
+    }
+    return [Paper(**my_paper), Paper(**my_paper)]
 
 
 @app.get("/paper/{paper_id}")
@@ -113,16 +103,18 @@ def read_paper_handler(paper_id: int):
 
 
 @app.put("/paper/{paper_id}")
-def update_paper_handler(paper_id: int):
-    return {
+def update_paper_handler(paper_id: int, paper: Paper):
+    my_paper = {
         "id": str(paper_id),
-        "author_id": [2, 5, 8],
-        "title": "my second paper",
-        "keywords": ["cloud"],
+        "author_id": [2, 5],
+        "title": "分散トレーシングのためのログ検索の高速化",
+        "keywords": ["分散", "トレーシング", "ログ", "検索", "高速化"],
+        "label": "CDSL-TR-051",
         "categories_id": [3],
-        "abstract": "this is a pen.",
-        "url": "https://example.com/yyy",
+        "abstract": "分散トレーシングは,マイクロサービスアーキテクチャでログによる動作の解析を実現する...",
+        "url": "https://drive.google.com/file/d/1feZlqWejgqf8zpWQBQOSpr5JXYkPQL4t/view",
         "thumbnail_url": "https://example.com/zzz",
-        "created_at": "1985-06-24T23:20:50.52Z",
-        "updated_at": "2021-04-18T23:20:50.52Z",
+        "created_at": datetime(1985, 6, 24, 23, 50, 52),
+        "updated_at": datetime(2021, 2, 4, 13, 52, 22)
     }
+    return Paper(**my_paper)
