e33faef6c27501bf79fd49e9cbded960ba1f3e11
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2021-09-02 10:49:24 +0900

    add class 'PaperCreateUpdate'

--- paper/main.py
+++ paper/main.py
@@ -19,7 +19,18 @@ db = client[MONGO_DBNAME]
 app = FastAPI()
 
 
-class Paper(BaseModel):
+class PaperCreateUpdate(BaseModel):
+    author_id: List[int]
+    title: str
+    keywords: List[str]
+    label: str
+    categories_id: List[int]
+    abstract: str
+    url: str
+    thumbnail_url: str
+
+
+class PaperRead(BaseModel):
     id: int
     author_id: List[int]
     title: str
@@ -50,9 +61,23 @@ def topz_handler():
 
 
 @app.post("/paper")
-def create_paper_handler(paper: Paper):
+def create_paper_handler(paper: PaperCreateUpdate):
     json_paper = jsonable_encoder(paper)
-    return Paper(**json_paper)
+    import random
+    my_paper = {
+        "id": random.randint(1, 99),
+        "author_id": json_paper.get("author_id"),
+        "title": json_paper.get("title"),
+        "keywords": json_paper.get("keywords"),
+        "label": json_paper.get("label"),
+        "categories_id": json_paper.get("categories_id"),
+        "abstract": json_paper.get("abstract"),
+        "url": json_paper.get("url"),
+        "thumbnail_url": json_paper.get("thumbnail_url"),
+        "created_at": datetime.now(),
+        "updated_at": datetime.now()
+    }
+    return PaperRead(**my_paper)
 
 
 @app.get("/paper")
@@ -70,13 +95,13 @@ def read_papers_handler():
         "created_at": datetime(1985, 6, 24, 23, 50, 52),
         "updated_at": datetime(2021, 2, 4, 13, 52, 22)
     }
-    return [Paper(**my_paper), Paper(**my_paper)]
+    return [PaperRead(**my_paper), PaperRead(**my_paper)]
 
 
 @app.get("/paper/{paper_id}")
 def read_paper_handler(paper_id: int):
     my_paper = {
-        "id": str(paper_id),
+        "id": paper_id,
         "author_id": [2, 5],
         "title": "分散トレーシングのためのログ検索の高速化",
         "keywords": ["分散", "トレーシング", "ログ", "検索", "高速化"],
@@ -88,22 +113,23 @@ def read_paper_handler(paper_id: int):
         "created_at": datetime(1985, 6, 24, 23, 50, 52),
         "updated_at": datetime(2021, 2, 4, 13, 52, 22)
     }
-    return Paper(**my_paper)
+    return PaperRead(**my_paper)
 
 
 @app.put("/paper/{paper_id}")
-def update_paper_handler(paper_id: int, paper: Paper):
+def update_paper_handler(paper_id: int, paper: PaperCreateUpdate):
+    json_paper = jsonable_encoder(paper)
     my_paper = {
-        "id": str(paper_id),
-        "author_id": [2, 5],
-        "title": "分散トレーシングのためのログ検索の高速化",
-        "keywords": ["分散", "トレーシング", "ログ", "検索", "高速化"],
-        "label": "CDSL-TR-051",
-        "categories_id": [3],
-        "abstract": "分散トレーシングは,マイクロサービスアーキテクチャでログによる動作の解析を実現する...",
-        "url": "https://drive.google.com/file/d/1feZlqWejgqf8zpWQBQOSpr5JXYkPQL4t/view",
-        "thumbnail_url": "https://example.com/zzz",
-        "created_at": datetime(1985, 6, 24, 23, 50, 52),
-        "updated_at": datetime(2021, 2, 4, 13, 52, 22)
+        "id": paper_id,
+        "author_id": json_paper.get("author_id"),
+        "title": json_paper.get("title"),
+        "keywords": json_paper.get("keywords"),
+        "label": json_paper.get("label"),
+        "categories_id": json_paper.get("categories_id"),
+        "abstract": json_paper.get("abstract"),
+        "url": json_paper.get("url"),
+        "thumbnail_url": json_paper.get("thumbnail_url"),
+        "created_at": datetime.now(),
+        "updated_at": datetime.now()
     }
-    return Paper(**my_paper)
+    return PaperRead(**my_paper)
