f8e67aff29e44f46b01bab2609506a251ae008bf
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2021-09-04 17:52:19 +0900

    authorをセットアップ

---
+++ author/main.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+from fastapi import FastAPI
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure, OperationFailure
+
+MONGO_USERNAME = os.getenv("MONGO_USERNAME", "root")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "example")
+MONGO_DBNAME = os.getenv("MONGO_DBNAME", "paper")
+MONGO_HOST = os.getenv("MONGO_HOST", "mongo")
+MONGO_CONNECTION_STRING = f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOST}/"
+
+client = MongoClient(MONGO_CONNECTION_STRING)
+try:
+    client.admin.command('ping')
+    print("MongoDB connected.")
+except (ConnectionFailure, OperationFailure) as e:
+    print("MongoDB not available. ", e)
+    sys.exit(-1)
+
+db = client[MONGO_DBNAME]
+app = FastAPI()
+
+
+@app.get("/")
+def root_handler():
+    return {"Hello": "World"}
+
+
+@app.get("/healthz")
+def healthz_handler():
+    return {"status": "ok", "message": "it works"}
+
+
+@app.get("/topz")
+def topz_handler():
+    return {"resource": "busy"}
