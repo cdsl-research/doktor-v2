558b51d3bca304285d67389adb2f3b308c8b88af
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2021-09-04 19:00:18 +0900

    Auhtorの追加，参照を実装

--- author/main.py
+++ author/main.py
@@ -1,7 +1,10 @@
 import os
 import sys
+from datetime import datetime
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure, OperationFailure
 
@@ -23,6 +26,31 @@ db = client[MONGO_DBNAME]
 app = FastAPI()
 
 
+class AuthorCreateUpdate(BaseModel):
+    first_name_ja: str
+    middle_name_ja: str
+    last_name_ja: str
+    first_name_en: str
+    middle_name_en: str
+    last_name_en: str
+    joined_year: int
+    is_graduated: bool
+
+
+class AuthorRead(BaseModel):
+    id: int
+    first_name_ja: str
+    middle_name_ja: str
+    last_name_ja: str
+    first_name_en: str
+    middle_name_en: str
+    last_name_en: str
+    joined_year: int
+    is_graduated: bool
+    created_at: datetime
+    updated_at: datetime
+
+
 @app.get("/")
 def root_handler():
     return {"Hello": "World"}
@@ -36,3 +64,39 @@ def healthz_handler():
 @app.get("/topz")
 def topz_handler():
     return {"resource": "busy"}
+
+
+@app.post("/author")
+def create_author_handler(author: AuthorCreateUpdate):
+    json_author = jsonable_encoder(author)
+    import random
+    my_author = {
+        "id": random.randint(1, 99),
+        "first_name_ja": json_author.get("first_name_ja"),
+        "middle_name_ja": json_author.get("middle_name_ja"),
+        "last_name_ja": json_author.get("last_name_ja"),
+        "first_name_en": json_author.get("first_name_en"),
+        "middle_name_en": json_author.get("middle_name_en"),
+        "last_name_en": json_author.get("last_name_en"),
+        "joined_year": json_author.get("joined_year"),
+        "is_graduated": json_author.get("is_graduated"),
+        "created_at": datetime.now(),
+        "updated_at": datetime.now()
+    }
+    insert_id = db["author"].insert_one(my_author).inserted_id
+    print("insert_id:", insert_id)
+    return AuthorRead(**my_author)
+
+
+@app.get("/author")
+def read_authors_handler():
+    return list(db["author"].find({}, {'_id': 0}))
+
+
+@app.get("/author/{author_id}")
+def read_author_handler(author_id: int):
+    entry = db["author"].find_one({"id": author_id}, {'_id': 0})
+    if entry:
+        return entry
+    else:
+        raise HTTPException(status_code=404, detail="Not Found")
