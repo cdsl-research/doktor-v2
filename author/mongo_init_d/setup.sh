79b6089a9fe40ce4e049aacf7bf17ee1ac8a095c
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2021-09-10 18:13:52 +0900

    renamed author's database dump

--- author/mongo_init_d/setup.sh
+++ author/mongo_init_d/setup.sh
@@ -1,5 +1,9 @@
-#!/bin/bash -xe
+#!/bin/bash
 
-mongoimport -u root -p example --db author --collection author \
+DATABASE_NAME=author
+COLLECTION_NAME=author
+MONGO_HOST=mongo
+
+mongoimport -u root -p example --db $DATABASE_NAME --collection $COLLECTION_NAME \
   --file /dump/dump.json --jsonArray \
-  --drop --authenticationDatabase admin --host mongo --port 27017
+  --drop --authenticationDatabase admin --host $MONGO_HOST --port 27017
