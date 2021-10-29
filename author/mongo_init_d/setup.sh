e89e63c25444a99c1a915f418f6b5010ad613659
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2021-10-30 01:01:24 +0900

    authorのdocker-composeを修正

--- author/mongo_init_d/setup.sh
+++ author/mongo_init_d/setup.sh
@@ -2,7 +2,7 @@
 
 DATABASE_NAME=author
 COLLECTION_NAME=author
-MONGO_HOST=mongo
+MONGO_HOST=author-mongo
 
 mongoimport -u root -p example --db $DATABASE_NAME --collection $COLLECTION_NAME \
   --file /dump/dump.json --jsonArray \
