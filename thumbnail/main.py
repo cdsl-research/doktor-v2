9d1b1f7e6cca4d35697ceeb08d27971630b1445b
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2022-04-05 21:53:20 +0900

    fix paper service host

--- thumbnail/main.py
+++ thumbnail/main.py
@@ -16,7 +16,7 @@ import requests
 import fitz
 
 """ Paper Service """
-PAPER_SVC_HOST = os.getenv("PAPER_SVC_HOST", "paper-app:4100")
+PAPER_SVC_HOST = os.getenv("PAPER_SVC_HOST", "paper-app:8000")
 
 """ Minio Setup"""
 MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "minio")
@@ -70,7 +70,8 @@ def topz_handler():
 def create_thumbnail(paper_uuid: UUID):
     # ファイルの取得
     try:
-        file_url = f"http://{PAPER_SVC_HOST}/paper/{paper_uuid}"
+        file_url = f"http://{PAPER_SVC_HOST}/paper/{paper_uuid}/download"
+        print("Fetch url:", file_url)
         pdf_data = requests.get(file_url)
     except Exception:
         raise HTTPException(status_code=400,
@@ -85,7 +86,7 @@ def create_thumbnail(paper_uuid: UUID):
                 x_ref = img[0]  # xref number
                 x_img = doc.extract_image(x_ref)
                 x_ext = x_img.get("ext")
-                filename = f"{p:02}-{j:02}.{x_ext}"
+                filename = f"{p}-{j}.{x_ext}"
                 write_file_buffer[filename] = x_img.get("image")
 
     # 画像の書き出し
@@ -97,20 +98,17 @@ def create_thumbnail(paper_uuid: UUID):
 
 
 @app.get("/thumbnail/{paper_uuid}/{image_id}")
-def read_thumbnail(paper_uuid: UUID, image_id: int):
+def read_thumbnail(paper_uuid: UUID, image_id: str):
+    # todo: image_id の形式のバリデーション
     try:
         obj_path = f"{paper_uuid}/{image_id}.png"
         response = minio_client.get_object(MINIO_BUCKET_NAME, obj_path)
-        res = response.read()
-        response.close()
-        response.release_conn()
-        return Response(content=res.read(), media_type="image/png")
+        # response.close()
+        return Response(content=response.read(), media_type="image/png")
     except Exception as e:
         res_status = 503
         res_message = "Internal Error"
-        if e.code in ("NoSuchKey", "NoSuchBucket", "ResourceNotFound"):
-            res_status = 404
-            res_message = "Not Found"
+        print("image fetch error:", e)
         raise HTTPException(status_code=res_status, detail=res_message)
 
 
