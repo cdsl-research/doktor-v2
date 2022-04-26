ccc7c336743fad6a5de2faa1e73199fdea437e3d
cdsl-research <cdsl@doktor-prod10>
2022-04-26 00:58:21 +0000

    update create_authors.py

---
+++ dev_tools/sample-data-uploader/create_authors.py
@@ -0,0 +1,42 @@
+import json
+
+"""
+{
+  "first_name_ja": "太郎",
+  "last_name_ja": "工科",
+  "first_name_en": "Taro",
+  "last_name_en": "Kouka",
+  "joined_year": 2019,
+  "graduation": true
+},
+"""
+def main():
+    with open("papers.json") as f:
+        papers = json.load(f)
+    # for p in papers:
+    #     print(p)
+    
+    """ Create unique author list from papers.json """
+    authors = set()
+    for p in papers:
+        authors = authors | set(p['author'])
+    # print(authors)
+    
+    write_buffer = []
+    for author in authors:
+        try:
+            first_name_ja, last_name_ja = author.split(' ')
+        except Exception:
+            print("Exception:", author)
+        write_buffer.append({
+            "first_name_ja": first_name_ja,
+            "last_name_ja": last_name_ja,
+        })
+
+    with open(f"_authors.{dt}.json", mode=w) as f:
+        json.dump(f, write_buffer)
+        
+
+if __name__ == "__main__":
+    main()
+
