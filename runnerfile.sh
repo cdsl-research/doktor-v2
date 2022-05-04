18955b7b8631db2c61f1b6b299ba3a9bca4d3e49
cdsl-research <cdsl@doktor-prod10>
2022-05-04 07:20:38 +0000

    add log writing

--- runnerfile.sh
+++ runnerfile.sh
@@ -13,3 +13,12 @@ task_down() {
         docker-compose -f $dir/docker-compose.yml down
     done
 }
+
+task_logging() {
+    mkdir logs/ || true
+    DIRS=$(find deploy/ -maxdepth 1 -mindepth 1 -type d | sed "s|deploy/||g")
+    for dir in $DIRS
+    do
+        kubectl logs deploy/${dir}-app-deploy -c istio-proxy -n ${dir} | grep HTTP > logs/${dir}-deploy.log
+    done
+}
