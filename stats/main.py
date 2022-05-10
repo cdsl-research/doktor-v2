f7f07d51723b78df2e1e14ee1e9845b2c47ef4bf
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2022-05-10 23:11:09 +0900

    統計機能を追加

--- stats/main.py
+++ stats/main.py
@@ -1,11 +1,13 @@
+from email import message
+from http.client import HTTPException
 import os
 import sys
 from datetime import datetime
-from typing import Optional
-from uuid import UUID, uuid4
+from uuid import UUID
 from ipaddress import IPv4Address
+from typing import Literal, Optional
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from pymongo import MongoClient
@@ -23,28 +25,46 @@ db = client[MONGO_DBNAME]
 app = FastAPI()
 
 
+class ServiceHello(BaseModel):
+    name: str
+
+
+class ServiceHealth(BaseModel):
+    resouce: str
+
+
+class StatusResponse(BaseModel):
+    status: Literal["ok", "error"]
+    message: Optional[str] = ""
+
+
 class StatsCreateUpdate(BaseModel):
     paper_uuid: UUID
     ip_v4_addr: IPv4Address
     timestamp: datetime
 
 
-@app.get("/")
+class StatsCount(BaseModel):
+    paper_uuid: UUID
+    total_downloads: int
+
+
+@app.get("/", response_model=ServiceHello)
 def root_handler():
-    return {"name": "stats"}
+    return ServiceHello(name="stats")
 
 
-@app.get("/healthz")
+@app.get("/healthz", response_model=StatusResponse)
 def healthz_handler():
-    return {"status": "ok", "message": "it works"}
+    return StatusResponse(status="ok", message="it works")
 
 
-@app.get("/topz")
+@app.get("/topz", response_model=ServiceHealth)
 def topz_handler():
-    return {"resource": "busy"}
+    return ServiceHealth(resource="busy")
 
 
-@app.post("/stats")
+@app.post("/stats", response_model=StatusResponse)
 def create_stats_handler(stats: StatsCreateUpdate):
     json_stats = jsonable_encoder(stats)
     my_stats = {
@@ -52,9 +72,25 @@ def create_stats_handler(stats: StatsCreateUpdate):
         "ip_v4_addr": str(json_stats.get("ip_v4_addr")),
         "timestamp": datetime.now(),
     }
-    insert_id = db["stats"].insert_one(my_stats).inserted_id
-    print("insert_id:", insert_id)
-    return {"status": "ok"}
+    try:
+        insert_id = db["stats"].insert_one(my_stats).inserted_id
+        print("insert_id:", insert_id)
+        return StatusResponse(status="ok", message=f"Success insert = {insert_id}")
+    except Exception:
+        raise HTTPException(status_code=500, detail="Fail to insert")
+
+
+@app.get("/stats/{paper_id}", response_model=StatsCount)
+def read_stats_handler(paper_id: UUID):
+    query = {
+        "paper_uuid": str(paper_id)
+    }
+    print("Stats Query:", query)
+    try:
+        downloads = db["stats"].find(query, {'_id': 0}).count()
+        return StatsCount(paper_uuid=paper_id, total_downloads=downloads)
+    except Exception:
+        raise HTTPException(status_code=500, detail="Fail to select")
 
 
 if __name__ == "__main__":
