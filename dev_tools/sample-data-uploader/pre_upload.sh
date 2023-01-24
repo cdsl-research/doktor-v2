46b9122b9707680fa466a1e8eae899ffde72d86a
tomoyk <tmyk.kym@gmail.com>
2023-01-24 16:10:43 +0000

    fix pre_upload

--- dev_tools/sample-data-uploader/pre_upload.sh
+++ dev_tools/sample-data-uploader/pre_upload.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
-set -xeu -o pipefail
+# set -xeu -o pipefail
 
 get_svc_port () {
-  echo $(kubectl get svc -n $1 | grep app | awk -F: '{print $2}' | grep -o '3[0-9]\+')
+    echo $(kubectl get svc -n $1 | grep app | awk -F: '{print $2}' | grep -o '3[0-9]\{4\}')
 }
 
 # kubectl get svc -n paper | grep app | awk -F: '{print $2}' | grep -o '3[0-9]\+'
@@ -15,7 +15,7 @@ result=$(get_svc_port "author")
 echo $result
 export AUTHOR_ENDPOINT="http://localhost:$result"
 
-result=$(get_svc_port "thumnail")
+result=$(get_svc_port "thumbnail")
 echo $result
 export THUMBNAIL_ENDPOINT="http://localhost:$result"
 
