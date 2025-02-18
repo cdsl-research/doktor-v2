a58ce58f0c5d30b7b2416c40aa977d7ff6e39f15
Tomoyuki KOYAMA <tomoyuki@koyama.me>
2025-02-18 09:51:05 +0900

    feat(dev_tools/sample-data-uploader): Implement a script to compare PDF files with paper.json and report missing files
    This commit introduces a new Python script that compares the set of PDF files in the current directory with the set of paper IDs in the "papers.json" file. The script prints out the names of any PDF files that are not found in the JSON file, as well as the reverse (paper IDs that do not have corresponding PDF files). This allows for easy identification and handling of missing or mismatched data.

---
+++ dev_tools/sample-data-uploader/diff.py
@@ -0,0 +1,16 @@
+import glob
+import json
+
+pdf_files = set(f.replace(".pdf", "") for f in glob.glob("*.pdf"))
+
+with open("papers.json") as f:
+    dat = json.load(f)
+    reg_files = set(p['paper_url_id'] for p in dat)
+
+print("Not found in paper.json")
+for l in pdf_files - reg_files:
+    print("\t", l, l+".pdf")
+
+print("PDF files are not found")
+for l in reg_files - pdf_files:
+    print("\t", l)
